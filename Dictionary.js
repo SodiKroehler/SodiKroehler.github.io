12c871209b996a29ca81fa95515391be0345e16d
Sodi Kroehler <sodikroehler.outlook.com>
2021-02-23 13:16:42 -0500

    First Commit

---
+++ Dictionary.js
@@ -0,0 +1,4 @@
+export Dictionary{
+  'length': 1;
+  1: ['Name: Nick Savarese';'Majors: Finance and Marketing','Cert: Business Analytics', 'Big: Maddie Macstudy', 'Littles: Mike Calderaro & Rach Linzer','Pledged: Fall 2018', 'Hometown: Garnet Valley, PA', 'Year: Junior'];
+}
---
+++ MMscript.js
@@ -0,0 +1,189 @@
+
+var Dictionary = {
+  0: 50,
+  1 : ['Nick Savarese','Majors: Finance and Marketing','Minor: none','Cert: Business Analytics', 'Big: Maddie Macstudy', 'Littles: Mike Calderaro & Rach Linzer','Pledged: Fall 2018', 'Hometown: Garnet Valley, PA', 'Year: Junior'],
+  2 : ['David Turnbo','Majors: Finance and BIS','Minor: none','Cert: Business Analytics', 'Big: Logan Ferdinand', 'Littles: Jared Deluccia & Rushil Ramprasad','Pledged: Fall 2019', 'Hometown: Silver Spring, MD', 'Year: Junior'],
+  3 : ['Jack Keeler','Majors: Accounting and BIS','Minor: Economics','Cert: Business Analytics and Leadership & Ethics', 'Big: Jared Lynch', 'Littles: none','Pledged: Fall 2019', 'Hometown: West Chester, PA', 'Year: Junior'],
+  4 : ['Meg Anderson','Majors: Accounting and Finance','Minor: none','Cert: Business Analytics and CPLE', 'Big: Steph Ott', 'Littles: Kashish Mehendiratta','Pledged: Fall 2019', 'Hometown: Murrysville, PA', 'Year: Sophomore'],
+  5 : ['Megan Nagy','Majors: Marketing and Supply Chain','Minor: Exercise Science', 'Cert: Sales', 'Big: Joanna Belechak', 'Littles: none','Pledged: Spring 2020', 'Hometown: Pittsburgh, PA', 'Year: Junior'],
+  6 : ['Angelo Giannotta','Majors: Accounting and Finance', 'Minor: Economics','Cert: Business Analytics and CPLE', 'Big: Aldo Ninassi', 'Littles: Kashish Mehendiratta','Pledged: Fall 2019', 'Hometown: Cranberry Township, PA', 'Year: Sophomore'],
+  7 : ['Gabby Cortez','Majors: Accounting and BIS','Minor: none','Cert: none', 'Big: Alexis Coggins', 'Littles: Gabby Policella, Alex Firestine, & Mariah Smith','Pledged: Spring 2019', 'Hometown: Miami, FL', 'Year: Junior'],
+  8 : ['Ryan OConnor','Majors: Finance and Marketing','Minor: Economy and Sociology', 'Big: Devin Bluemling', 'Littles: Jake Dachowski','Pledged: Fall 2019', 'Hometown: Murrysville, PA', 'Year: Sophomore'],
+  9 : ['Sabbona Abdulwhahab','Majors: Finance and Supply Chain','Minor: Economics','Cert: none', 'Big: Mehba Teshome', 'Littles: Joe McShea','Pledged: Fall 2019', 'Hometown: Atlanta, GA', 'Year: Junior'],
+  10 : ['Carlie Agentowicz','Majors: Business Information Systems & Nursing','Minor: none','Cert: Business Analytics', 'Big: Emma Skledar', 'Littles: none','Pledged: Spring 2020', 'Hometown: Wernersville, PA', 'Year: Junior'],
+  11 : ['John “Ladybug” Balafoutas','Majors: Finance, Accounting, & BIS','Minor: Economics','Cert: Business Analytics', 'Big: Julia Podvin', 'Littles: Dominic Furnari','Pledged: Fall 2018', 'Hometown: Mechanicsburg, PA', 'Year: Junior'],
+  12 : ['Julie Baranoski','Majors: Marketing and Supply Chain','Minor: none','Cert: CPLE', 'Big: Anoo Vaddi', 'Littles: none','Pledged: Spring 2020', 'Hometown: Langhorne, PA', 'Year: Sophomore'],
+  13 : ['Joanna Belechak','Majors: Marketing','Minor: Communications','Cert: none', 'Big: Silvia Bononi', 'Littles: Megan Nagy','Pledged: Fall 2019', 'Hometown: Cranberry Township, PA', 'Year: Junior'],
+  14 : ['Paige Bobka','Majors: Marketing & Supply Chain','Minor: none','Cert: Digital Media', 'Big: ___', 'Littles: Caroline Simms','Pledged: Fall 2018', 'Hometown: Kennett Square, PA', 'Year: Junior'],
+  15 : ['James Boston','Majors: Finance and Accounting','Minor: Economics','Cert: Business Analytics', 'Big: Devin Bluemling', 'Twin: Ryan O’Connor','Pledged: Fall 2020', 'Hometown: Upper St. Clair, PA', 'Year: Junior'],
+  16 : ['Nate Bristow','Majors: Accounting and Finance','Minor: Economics','Cert: none', 'Big: Russell Lloyd', 'Littles: none','Pledged: Fall 2019', 'FREE-NateBristow', 'Year: Sophomore'],
+  17 : ['Michael Calderaro','Majors: Accounting and BIS','Minor: Economics','Cert: none', 'Big: Ryan OConnor', 'Littles: none','Pledged: Fall 2020', 'Hometown: Washington, PA', 'Year: Sophomore'],
+  18 : ['Aaryanna Dawson-Sarabia','Majors: Finance and Supply Chain','Minor: French','Cert: CPLE', 'Big: Sara D’Andrea', 'Littles: Josie Stein','Pledged: Fall 2019', 'Hometown: Palos Verdes, CA', 'Year: Junior'],
+  19 : ['Luke Decker','Majors: Supply Chain and BIS','Minor: none','Cert: Business Analytics and Sports Management', 'Big: Benji Walters', 'Littles: none','Pledged: Spring 2020', 'Hometown: West Chester, PA', 'Year: Junior'],
+  20 : ['Milo DeFay','Majors: Marketing','Minor: none','Cert: none', 'Big: Jon Erdy', 'Littles: none','Pledged: Fall 2019', 'Hometown: Duncansville, PA', 'Year: Junior'],
+  21 : ['Jared Deluccia','Majors: Economics and History','Minor: none','Cert: Sustainability', 'Big: David Turnbo', 'Littles: none','Pledged: Spring 2020', 'Hometown: Berwyn, PA', 'Year: Junior'],
+  22 : ['Casey Eisenberg','Majors: BIS','Minor: Spanish','Cert: International Business', 'Big: Parker Alford', 'Littles: Shirley Hu & Tyler Kondash','Pledged: Fall 2017', 'Hometown: Highland Park, IL', 'Year: Senior'],
+  23 : ['Jon Erdy','Majors: Supply Chain and Marketing','Minor: none','Cert: none', 'Big: Owen Kelly', 'Littles: Russell Lloyd, Milo Defay & Jackson Schulte-Albert','Pledged: Fall 2018', 'Hometown: Media, PA', 'Year: Senior'],
+  24 : ['Emma Estabrook','Majors: Finance and BIS','Minor: none','Cert: none', 'Big: Blaire Roemer', 'Littles: Lauren McNichol','Pledged: Spring 2019', 'Hometown: Amherst, NH', 'Year: Senior'],
+  25 : ['Logan Ferdinand','Majors: Film and Media Studies','Minor: none','Cert: none', 'Big: Ben Pastoriust', 'Littles: David Turnbo','Pledged: Fall 2018', 'Hometown: Millburn, NJ', 'Year: Junior'],
+  26 : ['Alex Firestine','Majors: Finance and Accounting','Minor: Economics','Cert: Data Analytics and CPLE', 'Big: Gabby Cortes', 'Littles: John-Michael Yurisic','Pledged: Spring 2020', 'Hometown: Germantown, Maryland', 'Year: Sophomore'],
+  27 : ['Alyssa Fortes','Majors: BIS & Supply Chain','Minor: none','Cert: none', 'Big: Krista Vivian', 'Littles: Durga Ramachandran','Pledged: Fall 2018', 'Hometown: Gibsonia, PA', 'Year: Senior'],
+  28 : ['Dominic Furnari','Majors: Finance','Minor: Computer Science','Cert: none', 'Big: John Balafoutas', 'Littles: none','FREE-DominicFurnari', 'Hometown: Murrysville, PA', 'Year: Sophomore'],
+  29 : ['Will Greenwood','Majors: Finance','Minor: Economics','Cert: Business Analytics and CPLE', 'Big: Benji Walters', 'Littles: none','Pledged: Fall 2019', 'Hometown: McLean, VA', 'Year: Sophomore'],
+  30 : ['Brock Gresock','Majors: Accounting and Finance','Minor: none','Cert: none', 'Big: Kat Roland', 'Littles:  Matt Roberts & Andy Kubay','FREE-BrockGresock', 'FREE-BrockGresock', 'Year: Senior'],
+  31 : ['Juliana Hagen','Majors: Accounting and BIS','Minor: none','Cert: Business Analytics', 'Big: Abby Clarke', 'Littles: Sona Sharma','Pledged: Spring 2020', 'Hometown: Yardley, PA', 'Year: Junior'],
+  32 : ['Julia Hazen','Majors: Accounting, Finance & Marketing','Minor: Economics','Cert: none', 'Big: Ellie Stroh', 'Littles: none','Pledged: Fall 2019', 'Hometown: Arlington, MA', 'Year: Sophomore'],
+  33 : ['Evan Heilman','Majors: Finance','Minor: Economics and Legal Studies','Cert: CPLE', 'Big: Jackson Schulte-Albert', 'Littles: none','Pledged: Spring 2020', 'Hometown: Pittsburgh, PA', 'Year: Sophomore'],
+  34 : ['Sam Herbay','Majors: BIS & Supply Chain','Minor: none','Cert: none', 'Big: Jason Casperson', 'Littles: Matt Sumner','Pledged: Fall 2019', 'Hometown: Mt. Lebanon, PA', 'Year: Junior'],
+  35 : ['Kathleen Humphreys','Majors: Environmental Studies & Urban Studies','Minor: none','Cert: none', 'Big: Emma Francisco', 'Littles: Megan Parr & Sophie Kennedy','Pledged: Fall 2018', 'Hometown: Needham, MA', 'Year: Senior'],
+  36 : ['Jay Kanjirath','Majors: Marketing','Minor: none','Cert: none', 'Big: Audrey Layne', 'Littles: Evan Waldshmidt & Garret Clauser','Pledged: Spring 2019', 'Hometown: Vienna, VA', 'Year: Junior'],
+  37 : ['Sophie Kennedy','Majors: Nursing','Minor: none','Cert: Managing Health Services Programs and Projects', 'Big: Kathleen Humphreys', 'Littles: Anna Kovarick & Olivia Wagner','Pledged: Fall 2019', 'Hometown: Pittsburgh, PA', 'Year: Junior'],
+  38 : ['Saad Khan','Majors: Finance and Chemistry','Minor: Biomedical Engineering','Cert: none', 'Big: Rohil Chada', 'Littles: none','Pledged: Spring 2020', 'Hometown: Allentown, PA', 'Year: Sophomore'],
+  39 : ['Austin Kittredge','Majors: Finance','Minor: Economics','Cert: Business Analytics', 'Big: Andy Kubay', 'Littles: none','Pledged: Fall 2019', 'Hometown: Acton, MA', 'Year: Sophomore'],
+  40 : ['Riley Killian','Majors: Finance','Minor: none','Cert: Data Analytics', 'Big: Natalie Ung', 'Littles: none','FREE-RileyKillian', 'Hometown:  Pittsburgh, PA', 'Year: Sophomore'],
+  41 : ['Tyler Kondash','Majors: Marketing','Minor: Economics','Cert: Data Analytics', 'Big: Casey Eisenberg', 'Littles: none','Pledged: Spring 2020', 'Hometown: Nazareth, PA', 'Year: Sophomore'],
+  42 : ['Anna Kovarick','Majors: Business Administration & Economics','Minor: none','Cert: Public and Professional Writing', 'Big: Sophie Kennedy', 'Littles: Grace Wagner','Pledged: Spring 2020', 'Hometown: West Chester, PA', 'Year: Junior'],
+  43 : ['Andy Kubay','Majors: SCM & BIS','Minor: none','Cert: Business Analytics', 'Big: Brock Gresock', 'Littles: Jason Casperson & Austin Kittredge','Pledged: Fall 2018', 'Hometown: Gibsonia, PA', 'Year: Junior'],
+  44 : ['Russell Lloyd','Majors: Accounting and BIS','Minor: none','Cert: International Business', 'Big: Jon Erdy', 'Littles: Nate Bristow','Pledged: Spring 2019', 'Hometown: Howell, NJ', 'Year: Junior'],
+  45 : ['Jared Lynch','Majors: Finance','Minor: Economics','Cert: Business Analytics', 'Big: Adam White', 'Littles: Jack Keeler & and the legend of Spiteri','FREE-JaredLynch', 'FREE-JaredLynch', 'FREE-JaredLynch'],
+  46 : ['Nicole Mbuthia','Majors: Marketing and Supply Chain','Minor: none','Cert: none', 'Big: Maria Calderon', 'Littles: none','Pledged: Spring 2020', 'Hometown: East Stroudsburg, PA', 'Year: Sophomore'],
+  47 : ['Megan McCarthy','Majors: Marketing','Minor: Economics','Cert: CPLE & International Business', 'Big: Abby Coffman', 'Littles: none','Pledged: Spring 2020', 'Hometown: Pittsburgh, PA', 'Year: Sophomore'],
+  48 : ['Sarah McCartney','Majors: Marketing & Supply Chain','Minor: Administration of Justice','Cert: Corporate and Community Relations', 'Big: Alaina Donnelly', 'Littles: none','Pledged: Fall 2019', 'Hometown: Seattle, WA', 'Year: Sophomore'],
+  49 : ['Stella McDermot','Majors: Human Resources & Supply Chain Management','Minor: none','Cert: CPLE', 'Big: Ailish Fallon', 'Littles: none','Pledged: Fall 2019', 'Hometown: Acton, MA', 'Year: Sophomore'],
+  50 : ['Lauren McGarry','Majors: Accounting & BIS','Minor: none','Cert: Business Analytics & CPLE', 'Big: Nadine Alkaragholi', 'Littles: Helga Zeneli','FREE-LaurenMcGarry', 'Hometown: King of Prussia, PA', 'Year: Sophomore'],
+
+}
+
+class Brother {
+  constructor(){
+    this.name="";
+    this.id = 0;
+    this.children = ['','','',''];
+    this._correctNode;
+    this.totalNodes = Dictionary[0];
+
+    }
+
+
+  refresh () {
+    var rand = Math.floor(Math.random()*this.totalNodes)+1;
+    this.id= rand;
+    this.name=Dictionary[rand][0];
+  }
+
+  refreshChildren(k1,k2,k3,k4){
+    //get new kids
+    var picker = Math.floor(Math.random()*3);
+    //need to do one less, then add one so that name is never added as a category
+    var category = (Math.floor(Math.random()*8))+1;
+    this.children[picker] = Dictionary[this.id][category];
+    this._correctNode = picker;
+
+    for (var i=0;i<4;i++){
+      if (i != picker){
+
+        var randomBrother = Math.floor(Math.random()*this.totalNodes)+1;
+
+        /*while (Dictionary[randomBrother][category] == this.children[v]){
+          randomBrother = Math.floor(Math.random()*this.totalNodes)+1;
+        }*/
+
+        this.children[i] = Dictionary[randomBrother][category];
+
+      }
+    }
+    //send the new kids to school
+    return (this.children);
+  }
+  checkNode(ident){
+    var number = parseFloat(ident.slice(-1));
+    if (number == this._correctNode){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+
+var chosenNode = null;
+var _Parent = new Brother();
+var numCorrect;
+var _Kid_1;
+var _Kid_2;
+var _Kid_3;
+var _Kid_4;
+
+document.getElementById('start').addEventListener("click", function () {populateChildren()});
+document.getElementById('check').addEventListener("click", function () {validateChildren()});
+
+
+document.getElementById('kid1').addEventListener("click", function () {changeStatus('kid1')});
+document.getElementById('kid1').style.cursor = "pointer";
+document.getElementById('kid2').addEventListener("click", function () {changeStatus('kid2')});
+document.getElementById('kid2').style.cursor = "pointer";
+document.getElementById('kid3').addEventListener("click", function () {changeStatus('kid3')});
+document.getElementById('kid3').style.cursor = "pointer";
+document.getElementById('kid4').addEventListener("click", function () {changeStatus('kid4')});
+document.getElementById('kid4').style.cursor = "pointer";
+
+function populateChildren(){
+
+  refreshNodes();
+  document.getElementById('mother').innerHTML = _Parent.name;
+
+}
+
+function validateChildren() {
+  if (chosenNode != null){
+    var flag = _Parent.checkNode(chosenNode);
+  } else {
+    alert ('Please make a decision!');
+    return;
+  }
+
+  if (flag){
+    numCorrect++;
+    document.getElementById('numCorrect').innerHTML = numCorrect;
+    refreshNodes();
+  } else {
+    alert("That was not the correct answer. Gabby would not approve.");
+    refreshNodes();
+  }
+}
+
+function changeStatus(ident){
+  var _node = document.getElementById(ident);
+
+    document.getElementById('kid1').classList.add('off');
+    document.getElementById('kid2').classList.add('off');
+    document.getElementById('kid3').classList.add('off');
+    document.getElementById('kid4').classList.add('off');
+    document.getElementById('kid1').classList.remove('on');
+    document.getElementById('kid2').classList.remove('on');
+    document.getElementById('kid3').classList.remove('on');
+    document.getElementById('kid4').classList.remove('on');
+
+  if (_node.classList.contains('off')){
+
+    _node.classList.remove('off');
+    _node.classList.add('on');
+    chosenNode = ident;
+  } else {
+    _node.classList.remove('on');
+    _node.classList.add('off');
+    chosenNode = null;
+  }
+}
+
+function refreshNodes(){
+  _Parent.refresh();
+  console.log(_Parent);
+  var _children = _Parent.refreshChildren();
+
+  document.getElementById('kid1').innerHTML = _children[0];
+  document.getElementById('kid2').innerHTML = _children[1];
+  document.getElementById('kid3').innerHTML = _children[2];
+  document.getElementById('kid4').innerHTML = _children[3];
+}
