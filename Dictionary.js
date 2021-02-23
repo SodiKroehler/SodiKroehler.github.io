5e9343f00b4ff07c8481b3af8308af1715a1e701
Sodi Kroehler <60269118+SodiKroehler@users.noreply.github.com>
2021-02-23 13:29:58 -0500

    Delete Dictionary.js
    delete dictionary file

--- Dictionary.js
+++
@@ -1,4 +0,0 @@
-export Dictionary{
-  'length': 1;
-  1: ['Name: Nick Savarese';'Majors: Finance and Marketing','Cert: Business Analytics', 'Big: Maddie Macstudy', 'Littles: Mike Calderaro & Rach Linzer','Pledged: Fall 2018', 'Hometown: Garnet Valley, PA', 'Year: Junior'];
-}
