6fb362a7c91844ced1ad4c33e6db08e55da76b2d
Sodi Kroehler <60269118+SodiKroehler@users.noreply.github.com>
2023-01-07 14:33:36 -0500

    Update skScript.js

--- skScript.js
+++ skScript.js
@@ -1,14 +1,6 @@
-// let collectedWorksFile = require("./works.json")
-// import collectedWorksFile from './works.json' assert { type: 'JSON' };
+var collectedWorksFile = '{"works": [{"idx" : 1,"title": "Homo Stultus - for gods", "desc": "Homo Stultus, for gods is a collection of nine fictional pieces of academic writing (i.e. lectures, court rulings, patent filing forms, etc.), supposedly compiled in the year 3055 and time-shipped back to us, which address ethical, philosophical, and social governance issues from their day. While generally humorous and light-hearted, it sets out several novel views on morality and philosophy of mind, particularly in applications of artificial intelligences. It also includes three versions of the true, and as yet untold, story of a former member of the Twelve Tribes cult.","status": "Published", "b1link": "./files/IDIOTS-FinalManuscript.pdf", "b2link": "https://www.amazon.com/Homo-Stultus-Alexey-B-Crusoe/dp/B0BRGS8BKR/ref=sr_1_1?crid=260FVKQ9OA42J&keywords=homo+stultus&qid=1673115134&s=books&sprefix=homo+stultus%2Cstripbooks%2C62&sr=1-1"},{"idx" : 2,"title": "Analyzing the Effects of Digital Language Contact on Hindi Language Degradation","desc": "Sodi Kroehler, Sade Benjamin, Dr. Malihe Alikhani","status": "Pending","b1link": "./files/Kroehler_Benjamin_LanguageDegradation.pdf", "b2link": ""}]}'
 
- var collectedWorks;
- fetch('./works.json')
-     .then((response) => response.json())
-     .then((json) => collectedWorks = json);
-//var collectedWorksFile = '{"works": [{"idx" : 1,"title": "Homo Stultus - for gods", "desc": "Homo Stultus, for gods is a collection of nine fictional pieces of academic writing (i.e. lectures, court rulings, patent filing forms, etc.), supposedly compiled in the year 3055 and time-shipped back to us, which address ethical, philosophical, and social governance issues from their day. While generally humorous and light-hearted, it sets out several novel views on morality and philosophy of mind, particularly in applications of artificial intelligences. It also includes three versions of the true, and as yet untold, story of a former member of the Twelve Tribes cult.", "b1link": "http://cslabcms.nju.edu.cn/problem_solving/images/4/46/Problem_Solving_with_C%2B%2B_%289th_Savitch%29.pdf", "b2link": ""}]}';
-
-
-//var collectedWorks = JSON.parse(collectedWorksFile);
+var collectedWorks = JSON.parse(collectedWorksFile);
 for (work in collectedWorks){
   addWork(work);
 }
